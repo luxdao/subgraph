7a20300dfd7f9ce86e71c017456588fdb0d9d5e3
Adam Gall <galladamj@gmail.com>
2024-05-20 14:14:15 -0400

    When setting proposalTemplates or snapshotENS, if DAO object doesn't exist create one

--- src/key-value-pairs.ts
+++ src/key-value-pairs.ts
@@ -1,29 +1,23 @@
 import { log } from '@graphprotocol/graph-ts';
 import { ValueUpdated as ValueUpdatedEvent } from '../generated/KeyValuePairs/KeyValuePairs';
-import { DAO } from '../generated/schema';
+import { loadOrCreateDAO } from './shared';
 
 export function handleValueUpdated(event: ValueUpdatedEvent): void {
-  if (event.params.key == 'proposalTemplates') {
-    let dao = DAO.load(event.params.theAddress);
-    if (dao) {
-      log.info('Processing proposal templates for DAO: {}, the IPFS hash is: {}', [
-        event.params.theAddress.toHexString(),
-        event.params.value,
-      ]);
+  switch (event.params.key) {
+    case 'proposalTemplates': {
+      const dao = loadOrCreateDAO(event.params.theAddress);
       dao.proposalTemplatesHash = event.params.value;
       dao.save();
+      break;
     }
-  } else if (event.params.key == 'snapshotENS') {
-    let dao = DAO.load(event.params.theAddress);
-    if (dao) {
-      log.info('Processing Snapshot ENS for DAO: {}, the ENS is: {}', [
-        event.params.theAddress.toHexString(),
-        event.params.value,
-      ]);
+    case 'snapshotENS': {
+      const dao = loadOrCreateDAO(event.params.theAddress);
       dao.snapshotENS = event.params.value;
       dao.save();
+      break;
     }
-  } else {
-    log.warning('Unknown key: {}', [event.params.key]);
+    default: {
+      log.warning('Unknown key: {}', [event.params.key]);
+    }
   }
 }
