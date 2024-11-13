b98e01c30eab58f3147589fd0eb7c2b1f214ceb4
Adam Gall <galladamj@gmail.com>
2024-11-13 10:18:03 -0500

    Add support for dao name and child dao declarations on key value pairs contract

--- src/key-value-pairs.ts
+++ src/key-value-pairs.ts
@@ -1,6 +1,7 @@
 import { log } from '@graphprotocol/graph-ts';
 import { ValueUpdated as ValueUpdatedEvent } from '../generated/KeyValuePairs/KeyValuePairs';
 import { loadOrCreateDAO } from './shared';
+import { Bytes } from '@graphprotocol/graph-ts';
 
 export function handleValueUpdated(event: ValueUpdatedEvent): void {
   if (event.params.key == 'proposalTemplates') {
@@ -11,6 +12,22 @@ export function handleValueUpdated(event: ValueUpdatedEvent): void {
     const dao = loadOrCreateDAO(event.params.theAddress);
     dao.snapshotENS = event.params.value;
     dao.save();
+  } else if (event.params.key == 'daoName') {
+    const dao = loadOrCreateDAO(event.params.theAddress);
+    dao.name = event.params.value;
+    dao.save();
+  } else if (event.params.key == 'childDao') {
+    const subDAO = loadOrCreateDAO(Bytes.fromHexString(event.params.value));
+    if (subDAO.parentAddress !== null) {
+      return;
+    }
+
+    subDAO.parentAddress = event.params.theAddress;
+    subDAO.save();
+
+    const parentDAO = loadOrCreateDAO(event.params.theAddress);
+    parentDAO.hierarchy = parentDAO.hierarchy.concat([subDAO.id]);
+    parentDAO.save();
   } else {
     log.warning('Unknown key: {}', [event.params.key]);
   }
